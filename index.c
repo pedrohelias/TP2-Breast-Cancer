14f64825e4798920c79bedcb1a89f0bcddd4dfad
PedroMPagani <pedro.paulo.pagani@gmail.com>
2023-01-31 11:02:31 -0300

    commit reading pt1

--- index.c
+++ index.c
@@ -4,15 +4,31 @@
 
 typedef struct {
   int age;
+  int race; // 0 = white, 1 = black, 2 = other.
+  int marital; // 0 single, 1 married, 2 divorced
+  int status; // number of the status of the tumor
+  char tstage[2]; // t stage of the tumor;
+  char nstage[2]; // n stage of the tumor;
+  char sixthstage[6];
   int tumorSize;
+  int grade;
+  char differentiate[64];
+  char aStage[16];
+  int estrogenStatus; // 1 = positive, 0 negative
+  int prostegeroneStatus; // 1 positive 0 negative
+  int regionlNodeExamined;
+  int regionlNodePositive;
+  int survivalMonths;
+  int aliveStatus; // alive, dead;
+  int sortColumn;
 }, TumorData;
 
 int main(){
-
     char opcao, nomeArquivo[50];
     FILE *arquivo = NULL;
     long int posicao[962];
     TumorData* cache;
+    int dataSize = 1;
     while(opcao != '3'){
         printf("\n1 - Carregar Arquivo de Dados\n");
         printf("2 - Emitir Relatório\n");
@@ -30,19 +46,35 @@ int main(){
                     break;
                 }
                 char line[1024];
-                int dataSize = 1;
+                char race[16];
+                char marital[32];
+                char estrogenStatus[16];
+                char prostegeroneStatus[16];
+                char status[5];
                 while (fgets(line, 1024, arquivo)){
                    TumorData *new;
                    new = realloc(data,dataSize * sizeof *cache);
-                   if (new == NULL) {
+                   if (new == NULL){
                        perror("realloc");
                        free(cache);
                        return 2;
                    }
                    cache = new;
                    int index = dataSize - 1;
-                   int cnt = fscanf(fp,"%d,%d", &cache[index].age, &data[index].tumorSize);
-                   dataSize++;
+                   int cnt = fscanf(fp,"%d,%s,%s,%s,%s,%d,%s,%s,%d,%d,%d,%s",
+                    &cache[index].age,
+                    &race,
+                    &marital,
+                    &cache[index].tstage,
+                    &cache[index].nstage,
+                    &cache[index].sixthstage,&cache[index].grade,
+                    &cache[index].aStage,
+                    &cache[index].tumorSize,
+                    &estrogenStatus, &prostegeroneStatus,
+                    &cache[index].regionlNodeExamined,
+                    &cache[index].regionlNodePositive,
+                    &cache[index].survivalMonths, &status);
+                    dataSize++;
                    if (cnt == EOF) break;
                 }
                 break;
