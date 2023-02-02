5c88cbbacc8b06396b694646d3a0e571c5176af9
pedrohelias <pedrohelias95@hotmail.com>
2023-02-02 03:09:32 -0300

    teste

--- index.c
+++ index.c
@@ -3,6 +3,59 @@
 #include<string.h>
 
 
+
+
+// typedef struct {
+//   int age;
+//   int race; // 0 = white, 1 = black, 2 = other.
+//   int marital; // 2 single, 0 married, 1 divorced
+//   int status; // number of the status of the tumor
+//   char tstage[2]; // t stage of the tumor;
+//   char nstage[2]; // n stage of the tumor;
+//   char sixthstage[6];
+//   int tumorSize;
+//   int grade;
+//   char differentiate[64];
+//   char aStage[16];
+//   int estrogenStatus; // 1 = positive, 0 negative
+//   int prostegeroneStatus; // 1 positive 0 negative
+//   int regionlNodeExamined;
+//   int regionlNodePositive;
+//   int survivalMonths;
+//   int aliveStatus; // 0 = alive, 1 = dead;
+//   int sortColumn;
+//   struct TumorData * left;
+//   struct TumorData * right; 
+// } TumorData;
+
+
+
+
+
+
+FILE * carregarDatabase(char arquivo[50]){
+    FILE *file;
+    file = fopen(arquivo, "r");
+
+    if(file == NULL){
+        printf("Erro ao abrir o documento\n");
+        return 1;
+    }
+
+    return file;
+
+}
+
+
+
+
+TumorData tratarDados(FILE *arquivo, int *posicao){
+    TumorData arvore;
+
+}
+
+
+
 int main(){
 
     char opcao, nomeArquivo[50];
@@ -12,7 +65,7 @@ int main(){
 
     while(opcao != '3'){
 
-        printf("\n1 - Carregar Arquivo de Dados\n");
+        printf("\n1 - Inserir Base de Dados\n");
         printf("2 - Emitir Relatório\n");
         printf("3 - Fechar Aplicação\n\n");
 
@@ -26,12 +79,7 @@ int main(){
                 scanf("%s", nomeArquivo);
                 getchar();
 
-                arquivo = fopen(nomeArquivo, "r");
-                if(arquivo == NULL ){
-                    printf("Insira um arquivo válido, ocorreu um erro ao tentar ler o arquivo\n\n");
-                    break;
-                }
-
+                arquivo = carregarDatabase(nomeArquivo);
                 
                 break;
 
