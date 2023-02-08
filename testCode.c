987e724b4acb18711fd74ff5dbae77e559b07229
pedrohelias <pedrohelias95@hotmail.com>
2023-02-08 17:26:30 -0300

    almost 90% done

--- testCode.c
+++ testCode.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <stdbool.h>
 
-
 struct node {
   int data;
   int value;
@@ -24,6 +23,8 @@ struct node {
   struct node *left;
   struct node *right;
 };
+
+typedef struct node * Node;
 
 struct node *createNode(int data, int value, char *race, char marital, char tStage, char nStage, char sixStage, char diff, int grade, char aStage, char estrogStatus, char progesStatus, int regionalNodeExa, int regionalNodePos, int survivalMonths, char status){ // int regionalNodeExa, int regionalNodePos, int survivalMonths, char status) {
   struct node *newNode = (struct node *)malloc(sizeof(struct node));
@@ -83,6 +84,7 @@ void inOrder(struct node *root) {
   }
 }
 
+
 FILE * carregarDatabase(char arquivo[50]){
     FILE *file;
     file = fopen(arquivo, "r");
@@ -91,83 +93,54 @@ FILE * carregarDatabase(char arquivo[50]){
         printf("Erro ao abrir o documento\n");
         return 1;
     }
+    fclose(file);
 
-    return file;
+    return arquivo;
 
 }
 
-int main() {
+void free(Node Node){
+    if(Node == NULL)
+        return;
 
-  struct node *root = NULL;
-  char line[256];
-  char *token;
-  char *token1;
-  char *delim = ",";
-  char race[60];
-  char marital[60];
-  char tStage[60];
-  char nStage[60];
-  char sixStage[60];
-  char diff[60];
-  int grade;
-  char aStage[60];
-  char estrogStatus[60];
-  char progesStatus[60];
-  int regionalNodeExa;
-  int regionalNodePos;
-  int survivalMonths;
-  char status[40];
+    if(Node->left != NULL)
+        free(Node->left);
 
-  // char opcao, nomeArquivo[50];
-  // FILE *arquivo = NULL;
-  // long int posicao[962];
+    if(Node->right != NULL)
+        free(Node->right);
+    free(Node);
+}
 
 
-  // while(opcao != '3'){
-
-  //     printf("\n1 - Inserir Base de Dados\n");
-  //     printf("2 - Emitir Relatório\n");
-  //     printf("3 - Fechar Aplicação\n\n");
-
-  //     scanf("%c", &opcao);
-  //     getchar();
-
-  //     switch(opcao){
-  //         case '1':
-              
-  //             printf("\nInforme o nome do arquivo a ser lido:");
-  //             scanf("%s", nomeArquivo);
-  //             getchar();
-
-  //             arquivo = carregarDatabase(nomeArquivo);
-              
-  //             break;
-
-  //         case '2':
-  //             if(arquivo == NULL){
-  //                 printf("\nArquivo vazio\n\n");
-  //                 break;
-  //             }
-
-              
-  //             break;
-
-  //         case '3':
-  //             return 0;
-  //     }
-  // }
+void * processar(char arquivo[50]){
+    FILE *fp;
+    struct node *root = NULL;
+    char line[256];
+    char *token;
+    char *token1;
+    char *delim = ",";
+    char race[60];
+    char marital[60];
+    char tStage[60];
+    char nStage[60];
+    char sixStage[60];
+    char diff[60];
+    int grade;
+    char aStage[60];
+    char estrogStatus[60];
+    char progesStatus[60];
+    int regionalNodeExa;
+    int regionalNodePos;
+    int survivalMonths;
+    char status[40];
 
 
-
-
-
-  // Read the data from the CSV file
-  FILE *fp;
-  fp = fopen("Breast_Cancer_modified.csv", "r");
-  if (fp == NULL) {
-    printf("Could not open file");
-    return 1;
-  }
+    
+    fp = fopen(arquivo, "r");
+    if (fp == NULL) {
+      printf("Could not open file");
+      return 1;
+    }
 
   // int data;
   // int value;
@@ -272,6 +245,191 @@ int main() {
   fclose(fp);
 
   inOrder(root);
+
+
+
+}
+
+int main() {
+
+  struct node *root = NULL;
+  char line[256];
+  char *token;
+  char *token1;
+  char *delim = ",";
+  char race[60];
+  char marital[60];
+  char tStage[60];
+  char nStage[60];
+  char sixStage[60];
+  char diff[60];
+  int grade;
+  char aStage[60];
+  char estrogStatus[60];
+  char progesStatus[60];
+  int regionalNodeExa;
+  int regionalNodePos;
+  int survivalMonths;
+  char status[40];
+
+  char opcao, nomeArquivo[50];
+  FILE *arquivo = NULL;
+  long int posicao[962];
+
+
+  while(opcao != '3'){
+
+      printf("\n1 - Inserir Base de Dados\n");
+      printf("2 - Emitir Relatório\n");
+      printf("3 - Fechar Aplicação\n\n");
+
+      scanf("%c", &opcao);
+      getchar();
+
+      switch(opcao){
+          case '1':
+              
+              printf("\nInforme o nome do arquivo a ser lido:");
+              scanf("%s", nomeArquivo);
+              getchar();
+
+              root = carregarDatabase(nomeArquivo);
+              
+              break;
+
+          case '2':
+              if(arquivo == NULL){
+                  printf("\nArquivo vazio\n\n");
+                  break;
+              }
+
+              processar(root);
+                 
+              
+              break;
+
+          case '3':
+              fclose(arquivo);
+              free(root);
+              return 0;
+      }
+  }
+
+
+
+
+
+  // Read the data from the CSV file
+  FILE *fp;
+  fp = fopen("Breast_Cancer_modified.csv", "r");
+  if (fp == NULL) {
+    printf("Could not open file");
+    return 1;
+  }
+
+  // int data;
+  // int value;
+  // char race;
+  // char marital;
+  // char tStage;
+  // char nStage;
+  // char sixStage;
+  // char diff; 
+  // int grade;
+  // char aStage;
+  // char estrogStatus;
+  // char progesStatus;
+  // int regionalNodeExa;
+  // int regionalNodePos;
+  // int survivalMonths;
+  // char status;
+
+  // while (fgets(line, sizeof(line), fp) != NULL) {
+  //   line[strcspn(line, " \r\n")];
+  //   token = strtok(line, delim);
+  //   int data = atoi(token);
+  //   printf("%d ,", data);
+  
+  //   token = strtok(NULL, delim);
+  //   int value = atoi(token);
+  //   printf("%d, ", value);
+
+  //   token = strtok(NULL, delim);
+  //   //token = strcpy(token1, &line[2]);
+  //   char raceP = strcpy(race,token); //correto
+  //   printf("%s,", race);
+  //   // if (race == 92){
+  //   //     race = "white";
+  //   // }
+  //   token = strtok(NULL, delim);
+  //   char maritalP = strcpy(marital, token);
+  //   printf("%s, ", marital);
+
+  //   token = strtok(NULL, delim);
+  //   char tStageP = strcpy(tStage, token);
+  //   printf("%s, ", tStage);
+
+  //   token = strtok(NULL, delim);
+  //   char nStageP = strcpy(nStage, token);
+  //   printf("%s, ", nStage);
+
+  //   token = strtok(NULL, delim);
+  //   char  sixStageP = strcpy(sixStage, token); 
+  //   printf("%s, ", sixStage);
+   
+
+  //   token = strtok(NULL, delim);
+  //   char diffP = strcpy(diff, token);
+  //   printf("%s, ", diff);
+
+  //   token = strtok(NULL, delim);
+  //   int gradeP = atoi(token);
+  //   int ig = (int)gradeP;
+  //   printf("%d, ", ig);
+
+
+  //   token = strtok(NULL, delim);
+  //   char aStageP = strcpy(aStage, token);
+  //   printf("%s, ", aStage);
+
+  //   token = strtok(NULL, delim);
+  //   char estrogStatusP = strcpy(estrogStatus, token);
+  //   printf("%s, ", estrogStatus);
+
+  //   token = strtok(NULL, delim);    
+  //   char progesStatusP = strcpy(progesStatus, token);
+  //   printf("%s, ", progesStatus);
+
+  //   token = strtok(NULL, delim);
+  //   char regionalNodeExaP = atoi(token);
+  //   int ic = (int)regionalNodeExaP;
+  //   printf("%d,", ic);
+
+
+  //   token = strtok(NULL, delim);
+  //   char regionalNodePosP = atoi(token);
+  //   int ia = (int)regionalNodePosP;
+  //   printf("%d, ", ia);
+
+  //   token = strtok(NULL, delim);
+  //   char survivalMonthsP = atoi(token);
+  //   int ib = (int)survivalMonthsP;
+  //   printf("%d, ", ib);
+
+
+  //   token = strtok(NULL, delim);
+  //   char statusP = strcpy(status, token);
+  //   printf("%s \n", status);
+
+
+  
+
+  //   insert(&root, data, value, raceP, maritalP, tStageP, nStageP, sixStageP, diffP, gradeP, aStageP, estrogStatusP, progesStatusP, regionalNodeExaP, regionalNodePosP, survivalMonthsP, statusP); // nStageP, sixStageP, diffP, gradeP, aStageP, estrogStatusP, progesStatusP, regionalNodeExaP, regionalNodePosP, survivalMonthsP, statusP);
+  // }
+
+  // fclose(fp);
+
+  // inOrder(root);
 
   return 0;
 }
